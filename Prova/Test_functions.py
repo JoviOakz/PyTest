02861b0f7e4732ada94935a2f5533d4c25034afc
JoviOakz <joao.mateussdc@gmail.com>
2024-12-05 09:43:39 -0300

    fix

--- Prova/Test_functions.py
+++ Prova/Test_functions.py
@@ -1,23 +1,36 @@
+import pytest
 from Calculadora import Calculadora
 
-calculo = Calculadora()
+@pytest.fixture
+def calculo():
+    return Calculadora()
 
-def test_somar():
+def test_somar(calculo):
     assert calculo.somar(1, 2) == 3
     assert calculo.somar(1, 1) != 3
     assert calculo.somar(8, -2) == 6
 
-def test_subtrair():
+    # testes abaixo não possuem resultado verdadeiro
+    # assert calculo.somar(8, 2) == 7
+    # assert calculo.somar(2, 2) != 4
+
+def test_subtrair(calculo):
     assert calculo.subtrair(4, 2) == 2
     assert calculo.subtrair(1, 2) == -1
     assert calculo.subtrair(7, -2) == 9
 
-def test_dividir():
+    # testes abaixo não possuem resultado verdadeiro
+    # assert calculo.subtrair(72, -2) == 80
+    # assert calculo.subtrair(7, 8) != -1
+
+def test_dividir(calculo):
     assert calculo.dividir(1, 2) == 0.5
     assert calculo.dividir(3, 2) == 1.5
     assert calculo.dividir(8, 2) == 4
+    with pytest.raises(ValueError):  # Cenário negativo
+        calculo.dividir(6, 0)
     
-def test_multiplicar():
+def test_multiplicar(calculo):
     assert calculo.multiplicar(1, 2) == 2
     assert calculo.multiplicar(3, 2) == 6
     assert calculo.multiplicar(4, 2) == 8
